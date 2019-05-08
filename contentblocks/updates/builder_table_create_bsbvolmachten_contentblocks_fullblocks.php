93e19eb328f1a428accf9fcc2fabfa101d217287
Frenk Smeets <Dah@live.nl>
2019-05-08 18:45:51 +0200

    Add files via upload

---
+++ contentblocks/updates/builder_table_create_bsbvolmachten_contentblocks_fullblocks.php
@@ -0,0 +1,27 @@
+<?php namespace BsbVolmachten\ContentBlocks\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateBsbvolmachtenContentblocksFullblocks extends Migration
+{
+    public function up()
+    {
+        Schema::create('bsbvolmachten_contentblocks_fullblocks', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('title', 255);
+            $table->text('text');
+            $table->string('url', 255);
+            $table->string('url_text', 20);
+            $table->boolean('urlswitch');
+            $table->string('align', 255);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('bsbvolmachten_contentblocks_fullblocks');
+    }
+}
---
+++ contentblocks/updates/builder_table_update_bsbvolmachten_contentblocks_fullblocks_3.php
@@ -0,0 +1,23 @@
+<?php namespace BsbVolmachten\ContentBlocks\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateBsbvolmachtenContentblocksFullblocks3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('bsbvolmachten_contentblocks_fullblocks', function($table)
+        {
+            $table->string('url_text', 30)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('bsbvolmachten_contentblocks_fullblocks', function($table)
+        {
+            $table->string('url_text', 20)->change();
+        });
+    }
+}
